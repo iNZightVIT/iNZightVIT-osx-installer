8a67f7f9c8cd6b01acaad6d1aa1902eaf7dd8afc
Tom Elliott <tomelliottnz@gmail.com>
2018-08-16 10:40:01 +1200

    Remove R library; add clang6 and gfortran

---
+++ Installer/iNZightVIT/.tools/bin/gfortran/lib/gcc/x86_64-apple-darwin15/6.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-6.1.0/configure --prefix=/usr/local/gfortran --with-gmp=/Users/fx/devel/gcc/deps-static/x86_64 --enable-languages=c,c++,fortran,objc,obj-c++ --build=x86_64-apple-darwin15";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
